29de33dbb8472a7ee89eff258c0300d3c2cb817c
Jaime Liew <jaimeliew1@gmail.com>
2023-06-13 15:08:32 -0400

    reformulate yaw-induction equations as residuals and fixed-point iteration

--- mit_yaw_induction_wake_model/Rotor.py
+++ mit_yaw_induction_wake_model/Rotor.py
@@ -7,6 +7,19 @@ the induction, thrust and power of a yaw-misaligned actuator disk' Heck et al.
 import numpy as np
 
 
+def fixedpointiteration(f, x0, args=(), eps=0.000001, maxiter=100):
+    for _ in range(maxiter):
+        residuals = f(x0, *args)
+
+        x0 += residuals
+        if np.abs(residuals).max() < eps:
+            break
+    else:
+        raise ValueError("max iterations reached.")
+
+    return x0
+
+
 def yawthrustlimited(Ctprime, yaw, Uamb=1):
     """
     Solves the limiting case when v_4 << u_4. (Eq. 2.19, 2.20)
@@ -21,116 +34,26 @@ def yawthrustlimited(Ctprime, yaw, Uamb=1):
     return a, u4, v4
 
 
-def _yawthrustsubiteration(Ctprime, yaw, Uamb, a, u4, v4):
+def _yawthrust_residual(x, Ctprime, yaw, Uamb):
     """
-    Subiteration of Eq. 2.15
+    residual of Eq. 2.15
     """
-    _a = 1 - np.sqrt(Uamb**2 - u4**2 - v4**2) / (
-        np.sqrt(Ctprime) * Uamb * np.cos(yaw)
+    a, u4, v4 = x
+    e_a = (
+        1
+        - np.sqrt(Uamb**2 - u4**2 - v4**2)
+        / (np.sqrt(Ctprime) * Uamb * np.cos(yaw))
+        - a
     )
 
-    _u4 = Uamb * (1 - 0.5 * Ctprime * (1 - a) * np.cos(yaw) ** 2)
+    e_u4 = Uamb * (1 - 0.5 * Ctprime * (1 - a) * np.cos(yaw) ** 2) - u4
 
-    _v4 = -Uamb * 0.25 * Ctprime * (1 - a) ** 2 * np.sin(yaw) * np.cos(yaw) ** 2
+    e_v4 = -Uamb * 0.25 * Ctprime * (1 - a) ** 2 * np.sin(yaw) * np.cos(yaw) ** 2 - v4
 
-    return _a, _u4, _v4
-
-
-def yawthrust(Ctprime, yaw, Uamb=1, eps=0.000001):
-    """
-    Solves Eq. 2.15.
-    """
-    a_guess, u4_guess, v4_guess = yawthrustlimited(Ctprime, yaw, Uamb)
-
-    niter = 1
-    converged = False
-    while not converged:
-        a, u4, v4 = _yawthrustsubiteration(
-            Ctprime, yaw, Uamb, a_guess, u4_guess, v4_guess
-        )
-
-        if hasattr(a, "__iter__"):
-            a_error = np.linalg.norm((a - a_guess), ord=np.inf)
-            u4_error = np.linalg.norm((u4 - u4_guess), ord=np.inf)
-            v4_error = np.linalg.norm((v4 - v4_guess), ord=np.inf)
-        else:
-            a_error = np.abs(a - a_guess)
-            u4_error = np.abs(u4 - u4_guess)
-            v4_error = np.abs(v4 - v4_guess)
-
-        a_guess, u4_guess, v4_guess = a, u4, v4
-
-        niter += 1
-        converged = all(x < eps for x in [a_error, u4_error, v4_error])
-
-    return a, u4, v4
+    return np.array([e_a, e_u4, e_v4])
 
 
-def _yawthrust_ddCt_subiteration(dadCt, dudCt, dvdCt, Ct, yaw, a, u4, v4):
-    """
-    Calculate the derivatives of the variables dadCt, dudCt, and dvdCt.
-
-    Args:
-        dadCt: Current value of dadCt.
-        dudCt: Current value of dudCt.
-        dvdCt: Current value of dvdCt.
-        Ct: Value of Ct.
-        yaw: Value of yaw.
-        a: Value of a.
-        u4: Value of u4.
-        v4: Value of v4.
-
-    Returns:
-        Tuple: The new values of dadCt, dudCt, and dvdCt.
-    """
-    cosy, siny = np.cos(yaw), np.sin(yaw)
-    cosy2 = cosy**2
-
-    dadCt_new = ((1 - a) ** 2 * cosy2 + 2 * u4 * dudCt + 2 * v4 * dvdCt) / (
-        2 * Ct * (1 - a) * cosy2
-    )
-    dudCt_new = 0.5 * Ct * dadCt * cosy2 - 0.5 * (1 - a) * cosy2
-    dvdCt_new = (
-        0.5 * Ct * (1 - a) * dadCt * siny * cosy2 - 0.25 * (1 - a) ** 2 * siny * cosy2
-    )
-
-    return dadCt_new, dudCt_new, dvdCt_new
-
-
-def yawthrust_ddCt(Ct, yaw, a, u4, v4, eps=0.000001):
-    """
-    Calculate the derivatives of dadCt, dudCt, and dvdCt iteratively until convergence.
-
-    Args:
-        Ct: Value of Ct.
-        yaw: Value of yaw.
-        eps: Convergence threshold (default: 0.000001).
-
-    Returns:
-        Tuple: The values of dadCt, dudCt, and dvdCt.
-    """
-    dadCt, dudCt, dvdCt = 0, 0, 0
-
-    niter = 0
-    converged = False
-    while not converged:
-        dadCt_new, dudCt_new, dvdCt_new = _yawthrust_ddCt_subiteration(
-            dadCt, dudCt, dvdCt, Ct, yaw, a, u4, v4
-        )
-
-        dadCt_error = np.abs(dadCt - dadCt_new)
-        dudCt_error = np.abs(dudCt - dudCt_new)
-        dvdCt_error = np.abs(dvdCt - dvdCt_new)
-
-        dadCt, dudCt, dvdCt = dadCt_new, dudCt_new, dvdCt_new
-
-        niter += 1
-        converged = all(x < eps for x in [dadCt_error, dudCt_error, dvdCt_error])
-
-    return dadCt, dudCt, dvdCt
-
-
-def _yawthrust_ddyaw_subiteration(dadyaw, dudyaw, dvdyaw, Ct, yaw, a, u4, v4):
+def _yawthrust_ddyaw_residual(x, Ct, yaw, a, u4, v4):
     """
     Calculate the derivatives of the variables dadyaw, dudyaw, and dvdyaw.
 
@@ -147,12 +70,13 @@ def _yawthrust_ddyaw_subiteration(dadyaw, dudyaw, dvdyaw, Ct, yaw, a, u4, v4):
     Returns:
         Tuple: The new values of dadyaw, dudyaw, and dvdyaw.
     """
+    dadyaw, dudyaw, dvdyaw = x
     cosy, siny = np.cos(yaw), np.sin(yaw)
-    dadyaw_new = (u4 * dudyaw + v4 * dvdyaw - Ct * (1 - a) ** 2 * cosy * siny) / (
+    e_dadyaw = (u4 * dudyaw + v4 * dvdyaw - Ct * (1 - a) ** 2 * cosy * siny) / (
         Ct * (1 - a) * cosy**2
-    )
-    dudyaw_new = Ct * cosy * (0.5 * cosy * dadyaw + (1 - a) * siny)
-    dvdyaw_new = (
+    ) - dadyaw
+    e_dudyaw = Ct * cosy * (0.5 * cosy * dadyaw + (1 - a) * siny) - dudyaw
+    e_dvdyaw = (
         0.5
         * Ct
         * (1 - a)
@@ -161,14 +85,61 @@ def _yawthrust_ddyaw_subiteration(dadyaw, dudyaw, dvdyaw, Ct, yaw, a, u4, v4):
             - 0.5 * (1 - a) * cosy**3
             + (1 - a) * siny**2 * cosy
         )
-    )
+    ) - dvdyaw
 
-    return dadyaw_new, dudyaw_new, dvdyaw_new
+    return np.array([e_dadyaw, e_dudyaw, e_dvdyaw])
 
 
-def yawthrust_ddyaw(Ct, yaw, a, u4, v4, eps=0.000001):
+def _yawthrust_ddCt_residual(x, Ct, yaw, a, u4, v4):
     """
-    Calculate the derivatives of dayaw, duyaw, and dvyaw iteratively until convergence.
+    Calculate the derivatives of the variables dadCt, dudCt, and dvdCt.
+
+    Args:
+        dadCt: Current value of dadCt.
+        dudCt: Current value of dudCt.
+        dvdCt: Current value of dvdCt.
+        Ct: Value of Ct.
+        yaw: Value of yaw.
+        a: Value of a.
+        u4: Value of u4.
+        v4: Value of v4.
+
+    Returns:
+        Tuple: The new values of dadCt, dudCt, and dvdCt.
+    """
+    dadCt, dudCt, dvdCt = x
+    cosy, siny = np.cos(yaw), np.sin(yaw)
+    cosy2 = cosy**2
+
+    e_dadCt = ((1 - a) ** 2 * cosy2 + 2 * u4 * dudCt + 2 * v4 * dvdCt) / (
+        2 * Ct * (1 - a) * cosy2
+    ) - dadCt
+    e_dudCt = 0.5 * Ct * dadCt * cosy2 - 0.5 * (1 - a) * cosy2 - dudCt
+    e_dvdCt = (
+        0.5 * Ct * (1 - a) * dadCt * siny * cosy2 - 0.25 * (1 - a) ** 2 * siny * cosy2
+    ) - dvdCt
+
+    return np.array([e_dadCt, e_dudCt, e_dvdCt])
+
+
+def yawthrust(Ctprime, yaw, Uamb=1, eps=0.000001):
+    """
+    Solves Eq. 2.15.
+    """
+    _a, _u4, _v4 = yawthrustlimited(Ctprime, yaw, Uamb)
+
+    a, u4, v4 = fixedpointiteration(
+        _yawthrust_residual,
+        np.array([_a, _u4, _v4]),
+        args=(Ctprime, yaw, Uamb),
+        eps=eps,
+    )
+    return a, u4, v4
+
+
+def yawthrust_ddCt(Ct, yaw, a, u4, v4, eps=0.000001):
+    """
+    Calculate the derivatives of dadCt, dudCt, and dvdCt iteratively until convergence.
 
     Args:
         Ct: Value of Ct.
@@ -176,27 +147,34 @@ def yawthrust_ddyaw(Ct, yaw, a, u4, v4, eps=0.000001):
         eps: Convergence threshold (default: 0.000001).
 
     Returns:
-        Tuple: The values of dayaw, duyaw, and dvyaw.
+        Tuple: The values of dadCt, dudCt, and dvdCt.
     """
-    dayaw, duyaw, dvyaw = 0, 0, 0
+    x0 = np.array([0.0, 0.0, 0.0])
 
-    niter = 0
-    converged = False
-    while not converged:
-        dayaw_new, duyaw_new, dvyaw_new = _yawthrust_ddyaw_subiteration(
-            dayaw, duyaw, dvyaw, Ct, yaw, a, u4, v4
-        )
+    dadCt, dudCt, dvdCt = fixedpointiteration(
+        _yawthrust_ddCt_residual, x0, args=(Ct, yaw, a, u4, v4), eps=eps
+    )
+    return dadCt, dudCt, dvdCt
 
-        dayaw_error = np.abs(dayaw - dayaw_new)
-        duyaw_error = np.abs(duyaw - duyaw_new)
-        dvyaw_error = np.abs(dvyaw - dvyaw_new)
 
-        dayaw, duyaw, dvyaw = dayaw_new, duyaw_new, dvyaw_new
+def yawthrust_ddyaw(Ct, yaw, a, u4, v4, eps=0.000001):
+    """
+    Calculate the derivatives of dadyaw, dudyaw, and dvdyaw iteratively until convergence.
 
-        niter += 1
-        converged = all(x < eps for x in [dayaw_error, duyaw_error, dvyaw_error])
+    Args:
+        yaw: Value of yaw.
+        yaw: Value of yaw.
+        eps: Convergence threshold (default: 0.000001).
 
-    return dayaw, duyaw, dvyaw
+    Returns:
+        Tuple: The values of dadyaw, dudyaw, and dvdyaw.
+    """
+    x0 = np.array([0.0, 0.0, 0.0])
+
+    dadyaw, dudyaw, dvdyaw = fixedpointiteration(
+        _yawthrust_ddyaw_residual, x0, args=(Ct, yaw, a, u4, v4), eps=eps
+    )
+    return dadyaw, dudyaw, dvdyaw
 
 
 def gradyawthrust(Ct, yaw, eps=0.000001):
@@ -206,7 +184,3 @@ def gradyawthrust(Ct, yaw, eps=0.000001):
     dadyaw, dudyaw, dvdyaw = yawthrust_ddyaw(Ct, yaw, a, u4, v4, eps=eps)
 
     return a, u4, v4, dadCt, dudCt, dvdCt, dadyaw, dudyaw, dvdyaw
-
-
-if __name__ == "__main__":
-    pass
