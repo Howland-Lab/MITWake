105b940547d057269efc2432a1d023b7e167e22e
Jaime Liew <jaimeliew1@gmail.com>
2023-06-16 10:14:56 -0400

    vectorize LinearNiayifar

--- mit_yaw_induction_wake_model/Superposition.py
+++ mit_yaw_induction_wake_model/Superposition.py
@@ -24,6 +24,11 @@ class LinearNiayifar:
     def calculate_REWS(
         self, deficits, ddeficitdCts, ddeficitdyaws, REWS_method, windfarm
     ):
+        # Preintegrate the deficits
+        deficits = REWS_method.integrate(deficits)
+        ddeficitdCts = REWS_method.integrate(ddeficitdCts)
+        ddeficitdyaws = REWS_method.integrate(ddeficitdyaws)
+
         Xs = [turbine.x for turbine in windfarm.turbines]
         N = len(Xs)
         REWS = np.zeros_like(Xs, dtype=float)
@@ -33,22 +38,16 @@ class LinearNiayifar:
         upstream_idx = []
         # Iterate through turbines from upstream to downstream
         for idx in np.argsort(Xs):
-            # Calculate REWS of current turbine
-            U_rotor = np.ones_like(deficits[0, 0, :])
-            for idx_u in upstream_idx:
-                U_rotor -= REWS[idx_u] * deficits[idx_u, idx, :]
-            REWS[idx] = REWS_method.integrate(U_rotor)
+            REWS[idx] = 1 - np.sum(REWS * deficits[:, idx])
 
             # Determine the effect of each upstream turbine on current turbine
             for idx_u in upstream_idx:
-                dU_rotor_dCt = REWS[idx_u] * ddeficitdCts[idx_u, idx, ...]
-                dU_rotor_dyaw = REWS[idx_u] * ddeficitdyaws[idx_u, idx, ...]
-                for idx_uu in upstream_idx:
-                    dU_rotor_dCt += dREWSdCts[idx_u, idx_uu] * deficits[idx_uu, idx]
-                    dU_rotor_dyaw += dREWSdyaws[idx_u, idx_uu] * deficits[idx_uu, idx]
-
-                dREWSdCts[idx_u, idx] = -REWS_method.integrate(dU_rotor_dCt)
-                dREWSdyaws[idx_u, idx] = -REWS_method.integrate(dU_rotor_dyaw)
+                dREWSdCts[idx_u, idx] = -REWS[idx_u] * ddeficitdCts[
+                    idx_u, idx
+                ] - np.sum(dREWSdCts[idx_u, :] * deficits[:, idx])
+                dREWSdyaws[idx_u, idx] = -REWS[idx_u] * ddeficitdyaws[
+                    idx_u, idx
+                ] - np.sum(dREWSdyaws[idx_u, :] * deficits[:, idx])
 
             upstream_idx.append(idx)
         return REWS, dREWSdCts, dREWSdyaws
