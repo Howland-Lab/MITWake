fix wake calculation bug.

MITWake/Wake.py
@@ -11,7 +11,7 @@ class Gaussian:
         self.sigma = sigma  # Default values from paper
         self.kw = kw  # Default values from paper
 
-    def centerline(self, x: np.ndarray, dx=0.5) -> np.ndarray:
+    def centerline(self, x: np.ndarray, dx=0.05) -> np.ndarray:
         """
         Solves Eq. C4.
         """
@@ -30,7 +30,7 @@ class Gaussian:
         """
         Solves the normalized far-wake diameter (between C1 and C2)
         """
-        return 1 + self.kw * np.log(1 + np.exp(2 * x - 1))
+        return 1 + self.kw * np.log(1 + np.exp(2 * (x - 1)))
 
     def du(self, x: np.ndarray, wake_diameter: Optional[float] = None) -> np.ndarray:
         """
@@ -96,13 +96,13 @@ class GradGaussian(Gaussian):
         self.sigma = sigma  # Default values from paper
         self.kw = kw  # Default values from paper
 
-    def centerline(self, x: np.array, dx=0.5) -> Tuple[np.array, np.array, np.array]:
+    def centerline(self, x: np.array, dx=0.05) -> Tuple[np.array, np.array, np.array]:
         xmax = np.max(x)
         _x = np.arange(0, max(xmax, 2 * dx), dx)
         d = self.wake_diameter(_x)
 
         dv = -0.5 / d**2 * (1 + erf(_x / (np.sqrt(2) / 2)))
-        _yc_temp = cumtrapz(-dv, dx=dx, initial=0)
+        _yc_temp = cumtrapz(-dv, _x, initial=0)
 
         yc_temp = np.interp(x, _x, _yc_temp, left=0)
 

tests/test_wake_superposition.py
@@ -16,10 +16,8 @@ def test_windfarm_point_linear():
 
     Cp, _dCpdCt, _dCpdyaw = windfarm.turbine_Cp()
 
-    # np.testing.assert_array_almost_equal(REWS, [1.0, 0.60510138, 0.91372961])
-    np.testing.assert_array_almost_equal(REWS, [1.0, 0.58803785, 0.91372961])
-    # np.testing.assert_array_almost_equal(Cp, [0.52887482, 0.11001517, 0.45207377])
-    np.testing.assert_array_almost_equal(Cp, [0.52887482, 0.10096806, 0.45207377])
+    np.testing.assert_array_almost_equal(REWS, [1.0, 0.537983, 0.92791])
+    np.testing.assert_array_almost_equal(Cp, [0.528875, 0.077317, 0.473449])
 
 
 def test_windfarm_line_linear():
@@ -29,10 +27,8 @@ def test_windfarm_line_linear():
 
     Cp, _dCpdCt, _dCpdyaw = windfarm.turbine_Cp()
 
-    np.testing.assert_array_almost_equal(REWS, [1.0, 0.64027361, 0.88168824], decimal=5)
-    np.testing.assert_array_almost_equal(
-        Cp, [0.52887482, 0.13033618, 0.40616394], decimal=5
-    )
+    np.testing.assert_array_almost_equal(REWS, [1.0, 0.60569, 0.89368], decimal=5)
+    np.testing.assert_array_almost_equal(Cp, [0.52887, 0.11034, 0.42296], decimal=5)
 
 
 def test_windfarm_area_linear():
@@ -42,7 +38,5 @@ def test_windfarm_area_linear():
 
     Cp, _dCpdCt, _dCpdyaw = windfarm.turbine_Cp()
 
-    # np.testing.assert_array_almost_equal(REWS, [1.0, 0.63798711, 0.8892935])
-    np.testing.assert_array_almost_equal(REWS, [1.0, 0.69865796, 0.90144461])
-    # np.testing.assert_array_almost_equal(Cp, [0.52887482, 0.12894481, 0.41676533])
-    np.testing.assert_array_almost_equal(Cp, [0.52887482, 0.16934095, 0.43408358])
+    np.testing.assert_array_almost_equal(REWS, [1.0, 0.674466, 0.913121])
+    np.testing.assert_array_almost_equal(Cp, [0.528875, 0.152352, 0.45117])